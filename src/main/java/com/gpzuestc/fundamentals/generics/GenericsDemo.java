d12e6539d1605cdb56ea5d4cb1703fa64836f289
guopengzhang <guopeng.zhang2010@gmail.com>
2014-07-11 22:14:57 +0800

    generic test

--- src/main/java/com/gpzuestc/fundamentals/generics/GenericsDemo.java
+++ src/main/java/com/gpzuestc/fundamentals/generics/GenericsDemo.java
@@ -1,5 +1,11 @@
 package com.gpzuestc.fundamentals.generics;
 
+import org.junit.Test;
+
+import com.gpzuestc.User;
+import com.gpzuestc.constructor.Child;
+import com.gpzuestc.constructor.Parent;
+
 /**
  * @author gpzuestc
  * @date: 2013-10-27
@@ -16,9 +22,23 @@ class Point<T>{       // 此处可以随便写标识符号，T是type的简称
     }  
 };  
 public class GenericsDemo{  
+	
+	public static <T> T get(T obj){
+		return obj;
+	}
+	
+	
+	@Test
+	public void testT(){
+		System.out.println(get(new User("guopeng")));
+		
+		Parent p = get(new Child());
+	}
+	
     public static void main(String args[]){  
         Point<String> p = new Point<String>() ; // 里面的var类型为String类型  
         p.setVar("it") ;        // 设置字符串  
-        System.out.println(p.getVar().length()) ;   // 取得字符串的长度  
+        System.out.println(p.getVar().length()) ;   // 取得字符串的长度
+        
     }  
 };  
